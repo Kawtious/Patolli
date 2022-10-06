1b9f9d2a85f0bd577009961df8ef41125dbef51b
Kawtious <mewmewby2@gmail.com>
2022-10-05 21:45:38 -0700

    Made `Settings` an inner class of `Pregame`

--- src/patolli/game/configuration/Pregame.java
+++ src/patolli/game/configuration/Pregame.java
@@ -7,6 +7,7 @@ package patolli.game.configuration;
 import java.util.ArrayList;
 import java.util.Collections;
 import patolli.game.Player;
+import patolli.game.utils.Console;
 
 public class Pregame {
 
@@ -44,6 +45,90 @@ public class Pregame {
 
     public ArrayList<Player> getPlayers() {
         return players;
+    }
+
+    public static class Settings {
+
+        private int maxPlayers;
+
+        private int squares;
+
+        private int triangles;
+
+        private int bet;
+
+        private int maxTokens;
+
+        public Settings() {
+        }
+
+        public Settings(int maxPlayers, int squares, int triangles, int bet, int maxTokens) {
+            this.maxPlayers = maxPlayers;
+            this.squares = squares;
+            this.triangles = triangles;
+            this.bet = bet;
+            this.maxTokens = maxTokens;
+        }
+
+        public boolean validate() {
+            if (bet < 5) {
+                Console.WriteLine("Settings", "Bet has to be greater than 5 in order to play");
+                return false;
+            }
+
+            if (maxTokens < 2) {
+                Console.WriteLine("Settings", "Each player must have a minimum of 2 tokens to play");
+                return false;
+            }
+
+            if (squares < 2) {
+                Console.WriteLine("Settings", "Board must have a minimum of 2 common spaces per side of each blade");
+                return false;
+            }
+
+            return true;
+        }
+
+        public int getMaxPlayers() {
+            return maxPlayers;
+        }
+
+        public void setMaxPlayers(int maxPlayers) {
+            this.maxPlayers = maxPlayers;
+        }
+
+        public int getSquares() {
+            return squares;
+        }
+
+        public void setSquares(int squares) {
+            this.squares = squares;
+        }
+
+        public int getTriangles() {
+            return triangles;
+        }
+
+        public void setTriangles(int triangles) {
+            this.triangles = triangles;
+        }
+
+        public int getBet() {
+            return bet;
+        }
+
+        public void setBet(int bet) {
+            this.bet = bet;
+        }
+
+        public int getMaxTokens() {
+            return maxTokens;
+        }
+
+        public void setMaxTokens(int maxTokens) {
+            this.maxTokens = maxTokens;
+        }
+
     }
 
 }
