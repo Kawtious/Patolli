c2ea8ce21aef52170bcbe60340fc861411ab7cac
Kawtious <mewmewby2@gmail.com>
2022-10-07 01:54:00 -0700

    Implemented an entire IRC system

---
+++ src/patolli/game/online/server/Channel.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package patolli.game.online.server;
+
+import java.util.List;
+import java.util.UUID;
+import patolli.game.Game;
+import patolli.game.configuration.Pregame;
+import patolli.game.configuration.Pregame.Settings;
+import patolli.game.online.server.threads.SocketThread;
+
+public class Channel extends Connection {
+
+    private Pregame pregame;
+
+    private Game game;
+
+    private Group group;
+
+    /**
+     *
+     * @param group
+     * @param client
+     * @param operators
+     * @param name
+     */
+    public Channel(final Group group, final SocketThread client, final List<SocketThread> operators, final String name) {
+        pregame = new Pregame(new Settings());
+        id = UUID.randomUUID();
+        this.group = group;
+        this.name = name;
+        password = "";
+
+        client.setChannel(Channel.this);
+        client.setGroup(null);
+
+        add(client);
+        this.operators.addAll(operators);
+    }
+
+    /**
+     *
+     * @param group
+     * @param client
+     * @param operators
+     * @param name
+     * @param password
+     */
+    public Channel(final Group group, final SocketThread client, final List<SocketThread> operators, final String name, final String password) {
+        pregame = new Pregame(new Settings());
+        id = UUID.randomUUID();
+        this.group = group;
+        this.name = name;
+        this.password = auth.hash(password.toCharArray());
+
+        client.setChannel(Channel.this);
+        client.setGroup(null);
+
+        add(client);
+        this.operators.addAll(operators);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void destroy() {
+        if (!clients.isEmpty()) {
+            for (SocketThread client : clients) {
+                kick(client);
+            }
+        }
+
+        group.getChannels().remove(this);
+    }
+
+    /**
+     *
+     * @param client
+     */
+    @Override
+    public void kick(final SocketThread client) {
+        client.setChannel(null);
+        client.setGroup(group);
+        clients.remove(client);
+
+        if (clients.size() < 1) {
+            destroy();
+        }
+    }
+
+    /**
+     *
+     */
+    public void startGame() {
+        if (game != null) {
+            game = null;
+        }
+
+        for (SocketThread client : clients) {
+            pregame.add(client.getPlayer());
+        }
+
+        game = new Game(pregame);
+        game.init();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Group getGroup() {
+        return group;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Pregame getPregame() {
+        return pregame;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Game getGame() {
+        return game;
+    }
+
+}
--- src/patolli/game/online/server/threads/SocketStreams.java
+++ src/patolli/game/online/server/threads/SocketStreams.java
@@ -2,11 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package patolli.game.utils;
+package patolli.game.online.server.threads;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
 
 public final class SocketStreams {
 
@@ -56,6 +62,58 @@ public final class SocketStreams {
         if (len > 0) {
             dos.write(myByteArray, start, len);
         }
+    }
+
+    /**
+     *
+     * @param bytes
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static Object readObject(final byte[] bytes) throws IOException, ClassNotFoundException, ClassNotFoundException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInput in = new ObjectInputStream(bis);
+        return in.readObject();
+    }
+
+    /**
+     *
+     * @param object
+     * @return
+     * @throws IOException
+     */
+    public static byte[] readObjectBytes(final Object object) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(bos);
+        out.writeObject(object);
+        out.flush();
+
+        return bos.toByteArray();
+    }
+
+    /**
+     *
+     * @param dos
+     * @param object
+     * @throws IOException
+     */
+    public static void sendObject(final DataOutputStream dos, final Object object) throws IOException {
+        sendBytes(dos, readObjectBytes(object));
+    }
+
+    /**
+     *
+     * @param bytes
+     * @return
+     */
+    public static byte[] trim(final byte[] bytes) {
+        int i = bytes.length - 1;
+        while (i >= 0 && bytes[i] == 0) {
+            --i;
+        }
+
+        return Arrays.copyOf(bytes, i + 1);
     }
 
 }
