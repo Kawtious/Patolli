ba9d8d03ab2a792a07187e32280926dbc358da60
Kawtious <mewmewby2@gmail.com>
2022-10-07 17:46:57 -0700

    Updated console printing to look like Minecraft's
    because it looks fancy

--- src/patolli/game/utils/Console.java
+++ src/patolli/game/utils/Console.java
@@ -15,30 +15,24 @@ public class Console {
 
     public static void WriteLine(final String context, final String string) {
         LocalDateTime now = LocalDateTime.now();
-        int year = now.getYear();
-        int month = now.getMonthValue();
-        int day = now.getDayOfMonth();
         int hour = now.getHour();
         int minute = now.getMinute();
         int second = now.getSecond();
         int millis = now.get(ChronoField.MILLI_OF_SECOND); // Note: no direct getter available.
 
-        System.out.printf("[%d-%02d-%02d %02d:%02d:%02d.%03d] [%s] %s\n", year, month, day, hour, minute, second, millis, context, string);
+        System.out.printf("[%02d:%02d:%02d.%03d INFO]: [%s] %s\n", hour, minute, second, millis, context, string);
     }
 
     public static class Error {
 
         public static void WriteLine(final String context, final String string) {
             LocalDateTime now = LocalDateTime.now();
-            int year = now.getYear();
-            int month = now.getMonthValue();
-            int day = now.getDayOfMonth();
             int hour = now.getHour();
             int minute = now.getMinute();
             int second = now.getSecond();
             int millis = now.get(ChronoField.MILLI_OF_SECOND); // Note: no direct getter available.
 
-            System.err.printf("[%d-%02d-%02d %02d:%02d:%02d.%03d] [%s] %s\n", year, month, day, hour, minute, second, millis, context, string);
+            System.err.printf("[%02d:%02d:%02d.%03d ERROR]: [%s] %s\n", hour, minute, second, millis, context, string);
         }
 
     }
